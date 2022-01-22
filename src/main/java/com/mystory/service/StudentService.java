5b170f25d3be83027b681b93b96b55272482b1ee
wooho <woohobi@gmail.com>
2022-01-22 23:10:34 +0900

    도커 배포

--- src/main/java/com/mystory/service/StudentService.java
+++ src/main/java/com/mystory/service/StudentService.java
@@ -33,7 +33,7 @@ public class StudentService {
     private void validateDuplicationEmail(String email) {
         boolean isExist = studentRepository.existsStudentByEmail(email);
         if (isExist) {
-정            throw new BadRequestException("Email " + email + " is already signed");
+            throw new BadRequestException("Email " + email + " is already signed");
         }
     }
 
